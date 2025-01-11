6bfbb9263f13cddae2927b6b87eb65ee230f1a12
Yogiraj Mohan khaladkar <105957964+yogikh2005@users.noreply.github.com>
2025-01-11 21:34:14 +0530

    Create MergeList.cpp

---
+++ MergeList.cpp
@@ -0,0 +1,38 @@
+/*
+You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+
+Merge all the linked-lists into one sorted linked-list and return it.
+
+ 
+*/
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if (!l1) return l2;
+        if (!l2) return l1;
+
+        if (l1->val < l2->val) {
+            l1->next = mergeTwoLists(l1->next, l2);
+            return l1;
+        } else {
+            l2->next = mergeTwoLists(l1, l2->next);
+            return l2;
+        }
+    }
+
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if (lists.empty()) return nullptr;
+        return divideAndConquer(lists, 0, lists.size() - 1);
+    }
+
+    ListNode* divideAndConquer(vector<ListNode*>& lists, int left, int right) {
+        if (left == right) return lists[left];
+
+        int mid = left + (right - left) / 2;
+        ListNode* l1 = divideAndConquer(lists, left, mid);
+        ListNode* l2 = divideAndConquer(lists, mid + 1, right);
+        return mergeTwoLists(l1, l2);
+    }
+};
