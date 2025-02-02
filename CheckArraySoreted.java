9fe837ef44a9adef7f058fdee23feac3c07e8aa9
Yogiraj Mohan khaladkar <105957964+yogikh2005@users.noreply.github.com>
2025-02-02 20:02:48 +0530

    Create CheckArraySoreted.java

---
+++ CheckArraySoreted.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean check(int[] nums) {
+        int count = 0;
+        int n = nums.length;
+        
+        // Count the number of times nums[i] > nums[i+1]
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > nums[(i + 1) % n]) {
+                count++;
+            }
+        }
+        
+        // The array can be rotated sorted if count is at most 1
+        return count <= 1;
+    }
+}
+/*
+Given an array nums, return true if the array was originally sorted in non-decreasing order, then rotated some number of positions (including zero). Otherwise, return false.
+
+There may be duplicates in the original array.
+
+Note: An array A rotated by x positions results in an array B of the same length such that A[i] == B[(i+x) % A.length], where % is the modulo operation.
+
+ 
+
+Example 1:
+
+Input: nums = [3,4,5,1,2]
+Output: true
+Explanation: [1,2,3,4,5] is the original sorted array.
+You can rotate the array by x = 3 positions to begin on the the element of value 3: [3,4,5,1,2].*/
