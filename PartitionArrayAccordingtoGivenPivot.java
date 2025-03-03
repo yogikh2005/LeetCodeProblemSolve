9aa14e6d9f7c7e688e2a9225276cc0104973bc4e
Yogiraj Mohan khaladkar <105957964+yogikh2005@users.noreply.github.com>
2025-03-03 19:32:52 +0530

    Create PartitionArrayAccordingtoGivenPivot.java

---
+++ PartitionArrayAccordingtoGivenPivot.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+    public int[] pivotArray(int[] nums, int pivot) {
+        List<Integer> less = new ArrayList<>();
+        List<Integer> equal = new ArrayList<>();
+        List<Integer> greater = new ArrayList<>();
+        
+        // Categorize elements based on their relation to pivot
+        for (int num : nums) {
+            if (num < pivot) {
+                less.add(num);
+            } else if (num == pivot) {
+                equal.add(num);
+            } else {
+                greater.add(num);
+            }
+        }
+        
+        // Merge all lists back into nums
+        int index = 0;
+        for (int num : less) {
+            nums[index++] = num;
+        }
+        for (int num : equal) {
+            nums[index++] = num;
+        }
+        for (int num : greater) {
+            nums[index++] = num;
+        }
+        
+        return nums;
+    }
+}
