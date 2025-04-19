0492477f80ec78821cff9fc2177da63e048cc5e9
Yogiraj Mohan khaladkar <105957964+yogikh2005@users.noreply.github.com>
2025-04-19 12:27:30 +0530

    Create CountNumbeOfFairPairs.java

---
+++ CountNumbeOfFairPairs.java
@@ -0,0 +1,45 @@
+import java.util.Arrays;
+
+class Solution {
+    public long countFairPairs(int[] nums, int lower, int upper) {
+        Arrays.sort(nums);
+        long count = 0;
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            int left = lowerBound(nums, i + 1, n - 1, lower - nums[i]);
+            int right = upperBound(nums, i + 1, n - 1, upper - nums[i]);
+            if (left <= right) {
+                count += (right - left + 1);
+            }
+        }
+
+        return count;
+    }
+
+    // Finds the first index >= target
+    private int lowerBound(int[] nums, int start, int end, int target) {
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return start;
+    }
+
+    // Finds the last index <= target
+    private int upperBound(int[] nums, int start, int end, int target) {
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] <= target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return end;
+    }
+}
