f48f192cddb5a7e0caa6df03cc9909176075e9ed
Yogiraj Mohan khaladkar <105957964+yogikh2005@users.noreply.github.com>
2025-04-21 22:37:59 +0530

    Update CountHiddenSequences.java

--- CountHiddenSequences.java
+++ CountHiddenSequences.java
@@ -1,1 +1,14 @@
-(upper - max_prefix) - (lower - min_prefix) + 1
+class Solution {
+    public int numberOfArrays(int[] differences, int lower, int upper) {
+        long minPrefix = 0, maxPrefix = 0, prefixSum = 0;
+        for (int diff : differences) {
+            prefixSum += diff;
+            minPrefix = Math.min(minPrefix, prefixSum);
+            maxPrefix = Math.max(maxPrefix, prefixSum);
+        }
+        long minStart = lower - minPrefix;
+        long maxStart = upper - maxPrefix;
+
+        return (int)Math.max(0, maxStart - minStart + 1);
+    }
+}
