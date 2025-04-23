e7abec738edab86a748eb0db31bc1fc026e75e6b
Yogiraj Mohan khaladkar <105957964+yogikh2005@users.noreply.github.com>
2025-04-23 23:35:43 +0530

    Create CountLargestGroup.java

---
+++ CountLargestGroup.java
@@ -0,0 +1,32 @@
+import java.util.HashMap;
+
+class Solution {
+    public int countLargestGroup(int n) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int maxSize = 0;
+
+        for (int i = 1; i <= n; i++) {
+            int sum = digitSum(i);
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+            maxSize = Math.max(maxSize, map.get(sum));
+        }
+
+        int count = 0;
+        for (int size : map.values()) {
+            if (size == maxSize) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    private int digitSum(int num) {
+        int sum = 0;
+        while (num > 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+        return sum;
+    }
+}
