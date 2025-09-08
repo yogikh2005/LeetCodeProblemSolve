68e16f6c5948f9b3bb0db8b2f703bda4f5dad207
Yogiraj Mohan khaladkar <105957964+yogikh2005@users.noreply.github.com>
2025-09-08 23:36:22 +0530

    8/9/25

---
+++ IntegerToSumOfTwoNumber.java
@@ -0,0 +1,44 @@
+/*No-Zero integer is a positive integer that does not contain any 0 in its decimal representation.
+
+Given an integer n, return a list of two integers [a, b] where:
+
+a and b are No-Zero integers.
+a + b = n
+The test cases are generated so that there is at least one valid solution. If there are many valid solutions, you can return any of them.
+
+ 
+
+Example 1:
+
+Input: n = 2
+Output: [1,1]
+Explanation: Let a = 1 and b = 1.
+Both a and b are no-zero integers, and a + b = 2 = n.
+Example 2:
+
+Input: n = 11
+Output: [2,9]
+Explanation: Let a = 2 and b = 9.
+Both a and b are no-zero integers, and a + b = 11 = n.
+Note that there are other valid answers as [8, 3] that can be accepted.
+ 
+
+Constraints:
+
+2 <= n <= 104*/
+
+class Solution {
+    public int[] getNoZeroIntegers(int n) {
+        for (int a = 1; a < n; a++) {
+            int b = n - a;
+            if (isNoZero(a) && isNoZero(b)) {
+                return new int[]{a, b};
+            }
+        }
+        return new int[]{}; 
+    }
+
+    private boolean isNoZero(int num) {
+        return String.valueOf(num).indexOf('0') == -1;
+    }
+}
