add38364f3479295034b04c4cc0ec1536b36092f
Yogiraj Mohan khaladkar <105957964+yogikh2005@users.noreply.github.com>
2025-12-26 18:57:32 +0530

    26/12/25

---
+++ SumOfUniqueElement1748.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int sumOfUnique(int[] nums) {
+        int[] freq = new int[101];
+        
+        for (int num : nums) {
+            freq[num]++;
+        }
+        
+        int sum = 0;
+        for (int num : nums) {
+            if (freq[num] == 1) {
+                sum += num;
+            }
+        }
+        
+        return sum;
+    }
+}
