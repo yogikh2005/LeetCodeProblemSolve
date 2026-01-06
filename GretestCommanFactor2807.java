7a65f50716f0841fc5824f695a3debad4661514b
Yogiraj Mohan khaladkar <105957964+yogikh2005@users.noreply.github.com>
2026-01-06 23:51:44 +0530

    6/1/36

---
+++ GretestCommanFactor2807.java
@@ -0,0 +1,26 @@
+class Solution {
+    public ListNode insertGreatestCommonDivisors(ListNode head) {
+        ListNode curr = head;
+
+        while (curr != null && curr.next != null) {
+            int gcdValue = gcd(curr.val, curr.next.val);
+            ListNode newNode = new ListNode(gcdValue);
+
+            newNode.next = curr.next;
+            curr.next = newNode;
+
+            curr = newNode.next;
+        }
+
+        return head;
+    }
+
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
