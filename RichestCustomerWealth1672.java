d65274fb3656322512f5b1ccb90e2cb6dc8bf949
Yogiraj Mohan khaladkar <105957964+yogikh2005@users.noreply.github.com>
2026-01-09 21:57:23 +0530

    09/01/2026

---
+++ RichestCustomerWealth1672.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int maxWealth = 0;
+
+        for (int i = 0; i < accounts.length; i++) {
+            int sum = 0;
+            for (int j = 0; j < accounts[i].length; j++) {
+                sum += accounts[i][j];
+            }
+            maxWealth = Math.max(maxWealth, sum);
+        }
+
+        return maxWealth;
+    }
+}
