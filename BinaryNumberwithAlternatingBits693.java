659079cb465f65292ba23128d425731ac1052d35
Yogiraj Mohan khaladkar <105957964+yogikh2005@users.noreply.github.com>
2026-02-18 09:09:59 +0530

    18/02/2026
    Implement method to check for alternating bits in binary representation.

---
+++ BinaryNumberwithAlternatingBits693.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean hasAlternatingBits(int n) {
+        
+        String binary=Integer.toBinaryString(n);
+
+        boolean flag=true;
+
+        for(int i=0;i<binary.length()-1;i++)
+        {
+            if(binary.charAt(i)==binary.charAt(i+1))
+                {   
+                    flag=false;
+                }
+        }
+        return flag;
+    }
+}
